90bbe64f9421b7b921f1dc589e91a1d35e0ebbb1
Joe Titra <joseph.titra@harness.io>
2024-09-20 18:31:56 -0400

    updated

--- terraform/account_setup/resources.tf
+++ terraform/account_setup/resources.tf
@@ -8,12 +8,12 @@ locals {
 }
 
 import {
-  id = "projects/${var.proj_id}/locations/{{location}}/clusters/{{cluster_id}}"
+  id = "projects/${var.gcp_project_id}/locations/${var.gcp_zone}/clusters/${local.gke_cluster_name}"
   to = google_container_cluster.gke_cluster
 }
 
 import {
-  id = "{{project_id}}/{{location}}/{{cluster_id}}/{{pool_id}}"
+  id = "${var.gcp_project_id}/${var.gcp_zone}/${local.gke_cluster_name}/${local.gke_cluster_name}-pool-01"
   to = google_container_node_pool.gke_node_pool
 }
 

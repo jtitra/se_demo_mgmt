bb9b3e6966b0a75ded4eee3bfde0cc5c33ee82c5
Joe Titra <joseph.titra@harness.io>
2024-09-20 18:37:38 -0400

    updated

--- terraform/account_setup/variables.tf
+++ terraform/account_setup/variables.tf
@@ -28,3 +28,20 @@ variable "gcp_zone" {
   type    = string
   default = "us-east1-b"
 }
+
+// GKE Cluster & Node Pool
+variable "gke_min_node_count" {
+  type = string
+}
+
+variable "gke_max_node_count" {
+  type = string
+}
+
+variable "gke_machine_type" {
+  type = string
+}
+
+variable "resource_owner" {
+  type = string
+}
